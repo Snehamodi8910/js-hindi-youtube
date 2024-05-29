4d4b5003ee429c946e6e6426b61625cdf31b5d97
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-05-29 08:10:18 +0000

    configuration done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,4 +1,4 @@
-let score = true
+/* let score = true
 
  console.log(typeof score);//string
 console.log(typeof (score));//string 
@@ -21,8 +21,43 @@ console.log(booleanisLoggedIn);
     ""=>false
     "hitesh" => true */
 
-
+/* 
 let some = 33
 let stringNumber = String(some)
 console.log(stringNumber);  //prints 33 
-console.log(typeof stringNumber); //string
+console.log(typeof stringNumber); //string  */
+
+// ****************Operations***************
+
+let value =3
+let negValue = -value
+//console.log(negValue) -3
+ 
+let str1 = 'HELLO'
+let str2 = ' sneha'
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2"); // 12
+console.log("1" +2 ); //12
+console.log("1" +2 +2 ); //122
+console.log(1 +2 +"2" ); //32
+
+/* ToPrimitive (input , [pref type ] in article tces) */
+
+console.log(+true);//1
+console.log(+"");
+
+let num1, num2 ,num3
+num1 = num2 = num3 = 2+2  ///not preferred readibility most imp in coding
+
+let gamecounter = 100
+++gamecounter
+console.log(gamecounter); //read increment and decrement operators in mdn docs
+
+
+
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,17 @@
+/* console.log(2>1); -T
+console.log(2>=1);T
+console.log(2<1);F
+console.log(2<=1); F */
+
+/* console.log("2" >1);
+console.log("02">1); */
+
+console.log(null>0);//F
+console.log(null == 0);//F yr alag tarah se kaam krta hai than the >/>= 
+console.log(null>=0); //T NOT PREDOICTABLE null converted into 0
+//avoid such operatorions
+
+
+//===
+console.log("2" ===2); 
+//=== operator checks the data type also
