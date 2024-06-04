cfdc70d04d68012f8f15fbcdbf879a3aa91e0c32
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-06-04 19:20:27 +0000

    json

---
+++ 02_basics/05_objects3.js
@@ -0,0 +1,36 @@
+//destructuring
+
+const course = {
+    coursename:"js in hindi",
+    price: "1999",
+    instructor: "hitesh"
+}
+
+/* course.instructor
+ */
+//way to destructure
+const {instructor} = course
+console.log(instructor);
+
+/* const navbar= ({}) => {
+
+}
+navbar(company = "sneha") */
+
+//array destructuring also happens
+
+//APIs
+
+/* //apna kaam kisi aur ke sur pe daal dena
+{
+    name: "hitesh",
+    price: "free"
+} */
+
+[
+    {},
+    {},
+    {},
+]
+
+//json formatter tool
