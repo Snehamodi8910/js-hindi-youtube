42406ed4261c237786716451ac61ae00c64e442d
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-06-10 20:01:58 +0000

    while loop

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,14 @@
+//while loop
+let i = 0 
+while (i<=10) {
+/*     console.log(`value of index is ${i}`);
+ */    i +=2
+}
+
+//do whilw
+let score =11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);//kaam pehle hota hai fr loop 
+//to 11 will be the output 
