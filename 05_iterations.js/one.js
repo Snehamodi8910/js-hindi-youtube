c492d60b82b8bf8ec27560634647764600e4e0f0
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-06-10 19:56:58 +0000

    for loop

---
+++ 05_iterations.js/one.js
@@ -0,0 +1,42 @@
+//for
+/* 
+for (let index = 0; index < 10; index++) {
+    const element = index; 
+    if(element == 5) */ //console.log("5 is the best number");
+/*     console.log(element);
+ */
+//index initialise hoga 
+//condition check 
+/* 
+for(let i = 1; i<= 10 ; i++)
+{
+    console.log(`outer loop value : ${i}`);
+    for(let j = 1 ; j<=10 ; j++)
+        {
+           console.log( `${i} *  ${j} = ${i*j}`);
+           
+            //console.log(`inner loop value : ${j} and inner loop ${i}` )
+        }
+} */
+/* 
+        let array = ["flash" , "superman" , "batman"]
+
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            console.log(element);
+        } */
+
+        //break and continue 
+/* 
+        for (let index = 1; index <= 20; index++) {
+            if(index==5){console.log(`value 5 detected`) ; break;}
+           console.log(`value of i ${index}`); //break mai loop se bahar detect ke baad 
+            
+        }
+
+        for (let index = 1; index <= 20; index++) {
+            if(index==5){console.log(`value 5 detected`) ; continue;}
+           console.log(`value of i ${index}`); // 5 print nhi hua as 5 skip hoga
+            
+        }
+ */
