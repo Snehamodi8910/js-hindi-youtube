0c6c913d5f83108efb51ec52359e3e17af937987
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-06-10 20:50:21 +0000

    chaining , filter , map

---
+++ 05_iterations.js/six.js
@@ -0,0 +1,35 @@
+//const coding = ["js" , "pytho" , "cpp"]
+
+/* const values = coding.forEach((item) => {
+    //console.log(item);
+    return item
+})
+ */
+//console.log(values);//UNDEFINED 
+
+const mynums = [1 , 2 ,3 ,4 ,5 ,6,7,8,9,10 ]
+//filter
+//const newnums = mynums.filter( (num) => { return num > 4}) //donot forget to use return 
+/* const newnums = mynums.filter( (num) => num>4)  */// 5 6 7 8 9 10
+//console.log(newnums);
+
+/* const newnums = []
+mynums.forEach(  (num) => {
+    if(num>4) newnums.push(num) //same hihe bs logic lagana pda
+
+})
+console.log(newnums) */
+
+
+//map use krke
+//const newnums = mynums.map( (num) => {return num+10})
+//console.log(newnums);
+
+//chaining
+
+/* const newnums = mynums
+                    .map( (num)=> num*10)//ye jo res aaya vo sec mai pass hoga
+                    .map( (num)=> num+1 )
+                    .filter((num)=> num>=40)
+
+console.log(newnums); */
