af89c974c525b435dd56008c3ee4822eeae53dbe
Sneha Modi <117431619+Snehamodi8910@users.noreply.github.com>
2024-06-21 21:39:53 +0000

    promise in js

---
+++ 09_Advaced_one/03_promises.js
@@ -0,0 +1,10 @@
+fetch('https://github.com/hiteshchoudhary/apihub')
+.then((response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch((error)=>{
+    console.log(error);
+})
